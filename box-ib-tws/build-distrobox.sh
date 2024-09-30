2014207f171e1bb041cc2da0d18c6ad776d32020
Alexandros Karypidis <karypid@gmail.com>
2024-09-30 14:26:10 +0100

    Fix hard-coded container names to use variables

--- box-ib-tws/build-distrobox.sh
+++ box-ib-tws/build-distrobox.sh
@@ -7,8 +7,8 @@ podman image build -t "$_container" . && \
     DBX_CONTAINER_MANAGER=podman DBX_CONTAINER_ALWAYS_PULL=0 distrobox create --image $_container:latest --name $_container
 
 # Actual installation using logged in account's uid/gid
-distrobox enter -n dbox-ib-tws -- sh /container/configure-container.sh stable
-distrobox enter -n dbox-ib-tws -- sh /container/configure-container.sh latest
+distrobox enter -n "$_container" -- sh /container/configure-container.sh stable
+distrobox enter -n "$_container" -- sh /container/configure-container.sh latest
 
 # export apps to host
 # distrobox enter -n "$_container" -- distrobox-export --app Citrix
--- box-ib-tws/build-toolbox.sh
+++ box-ib-tws/build-toolbox.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-_container=${1:-dbox-ib-tws}
+_container=${1:-tbox-ib-tws}
 echo "Building image/container: $_container"
 
 podman image build -t "$_container" . && \
     toolbox create -i "$_container:latest" "$_container"
 
 # Actual installation using logged in account's uid/gid
-toolbox run -c tbox-ib-tws -- sh /container/configure-container.sh stable
-toolbox run -c tbox-ib-tws -- sh /container/configure-container.sh latest
+toolbox run -c "$_container" -- sh /container/configure-container.sh stable
+toolbox run -c "$_container" -- sh /container/configure-container.sh latest
 
 # export apps to host
 # sh export-tbox.sh "$_container"
