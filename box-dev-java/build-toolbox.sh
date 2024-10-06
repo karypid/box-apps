36d89e56a623bc457187459422c49310973a37ef
Alexandros Karypidis <karypid@gmail.com>
2024-10-06 13:57:33 +0100

    Add Java developer box

---
+++ box-dev-java/build-toolbox.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+_container=${1:-tbox-dev-java}
+echo "Building image/container: $_container"
+
+podman image build -t "$_container" . && \
+	toolbox create -i "$_container:latest" "$_container"
+
