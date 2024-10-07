23e0c51aaf94343a1dd8ee4c7e9e88b359b97f6f
Alexandros Karypidis <karypid@gmail.com>
2024-10-07 13:55:33 +0100

    Reorganize ib-tws box

--- box-dev-java/container/configure-container.sh
+++ box-dev-java/container/configure-container.sh
@@ -4,5 +4,9 @@ sudo mkdir -p /opt/sdk
 sudo chown "$(id -un)":"$(id -gn)" /opt/sdk
 export SDKMAN_DIR="/opt/sdk/sdkman" && curl -s "https://get.sdkman.io" | bash
 chsh -s /bin/bash
-bash -l -c " sdk install java 21.0.4-tem ; sdk version"
+bash -l -c "sdk install java 21-local /usr/lib/jvm/java-21-openjdk-amd64"
+bash -l -c "sdk install maven 3.8-local /usr/share/maven"
+bash -l -c "sdk default java 21-local"
+bash -l -c "sdk default maven 3.8-local"
+bash -l -c "sdk version ; sdk current"
 
