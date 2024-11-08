7cde224e975a9486928ecbbd336c921a32c924a8
Alexandros Karypidis <1221101+karypid@users.noreply.github.com>
2024-11-08 02:21:08 +0000

    Force X11 backend as it crashes on Wayland

--- box-ib-tws/build-toolbox.sh
+++ box-ib-tws/build-toolbox.sh
@@ -12,9 +12,9 @@ toolbox run -c "$_container" -- sh /container/configure-container.sh latest
 
 # export apps to host
 ../common/create-desktop-entry.sh --container $_container \
-	--exec "/inst/tws/stable/tws -J-DjtsConfigDir=/home/karypid@ad.home.lan/Jts %U" \
+	--exec "sh -c \"GDK_BACKEND=x11 /inst/tws/stable/tws -J-DjtsConfigDir=/home/karypid@ad.home.lan/Jts %U\"" \
 	--icon /inst/tws/stable/.install4j/tws.png --name "IB TWS 10.19" --wmclass install4j-jclient-LoginFrame
 ../common/create-desktop-entry.sh --container $_container \
-	--exec "/inst/tws/latest/tws -J-DjtsConfigDir=/home/karypid@ad.home.lan/Jts %U" \
+	--exec "sh -c \"GDK_BACKEND=x11 /inst/tws/latest/tws -J-DjtsConfigDir=/home/karypid@ad.home.lan/Jts %U\"" \
 	--icon /inst/tws/latest/.install4j/tws.png --name "IB TWS 10.31" --wmclass install4j-jclient-LoginFrame
 
