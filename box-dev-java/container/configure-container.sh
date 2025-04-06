8030667e55d4c03252e6bd671e599572e72a5814
Alexandros Karypidis <1221101+karypid@users.noreply.github.com>
2025-04-06 22:16:44 +0100

    Update to version 2025-03

--- box-dev-java/container/configure-container.sh
+++ box-dev-java/container/configure-container.sh
@@ -2,11 +2,12 @@
 
 sudo mkdir -p /opt/sdk
 sudo chown "$(id -un)":"$(id -gn)" /opt/sdk
-export SDKMAN_DIR="/opt/sdk/sdkman" && curl -s "https://get.sdkman.io" | bash
+echo Installing SDKMAN
+export SDKMAN_DIR="/opt/sdk/sdkman" && curl -s "https://get.sdkman.io?rcupdate=false" | bash
 chsh -s /bin/bash
-bash -l -c "sdk install java 21-local /usr/lib/jvm/java-21-openjdk-amd64"
-bash -l -c "sdk install maven 3.8-local /usr/share/maven"
-bash -l -c "sdk default java 21-local"
-bash -l -c "sdk default maven 3.8-local"
-bash -l -c "sdk version ; sdk current"
+bash -l -c ". /opt/sdk/sdkman/bin/sdkman-init.sh ; sdk install java 21-local /usr/lib/jvm/java-21-openjdk-amd64"
+bash -l -c ". /opt/sdk/sdkman/bin/sdkman-init.sh ; sdk install maven 3.8-local /usr/share/maven"
+bash -l -c ". /opt/sdk/sdkman/bin/sdkman-init.sh ; sdk default java 21-local"
+bash -l -c ". /opt/sdk/sdkman/bin/sdkman-init.sh ; sdk default maven 3.8-local"
+bash -l -c ". /opt/sdk/sdkman/bin/sdkman-init.sh ; sdk version ; sdk current"
 
