2c4dbec9788898d72ba882f1fcd0258f4507ae89
Alexandros Karypidis <1221101+karypid@users.noreply.github.com>
2025-12-05 00:33:28 +0000

    Drop init system parameter
    It is not needed, and the toolbox image does not contain it anyway

--- box-nixos/build.sh
+++ box-nixos/build.sh
@@ -1,7 +1,7 @@
 
 rm -fr ~/bx/mybox/{.profile,.bash*,.zsh*,.zprofile,.zcompdump,.nix*,.config,.local,.cache}
 distrobox rm -f mybox
-distrobox create -i quay.io/fedora/fedora-toolbox:43 --name mybox --home ~/bx/mybox --init
+distrobox create -i quay.io/fedora/fedora-toolbox:43 --name mybox --home ~/bx/mybox
 
 cp insert_imports.sh ~/bx/mybox/
 cp -R dotfiles ~/bx/mybox/
